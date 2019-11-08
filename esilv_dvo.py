69e8db8435eb2edd88cec6c56f0d9ba46ce068b2
Jules Aubert <aubertjules@gmail.com>
2019-11-08 15:57:54 +0100

    Connection to DVO: OK.

--- esilv_dvo.py
+++ esilv_dvo.py
@@ -13,49 +13,33 @@ def connection():
     browser = webdriver.Firefox()
     url = "https://devinci-online.brightspace.com"
     browser.get(url)
-    username_form = browser.find_element_by_id("id_username")
-    password_form = browser.find_element_by_id("id_password")
+    username_form = browser.find_element_by_id("userNameInput")
+    password_form = browser.find_element_by_id("passwordInput")
     username_form.send_keys(username)
     password_form.send_keys(password)
-    submit_button = browser.find_element_by_class_name("mdl-button")
+    submit_button = browser.find_element_by_id("submitButton")
     submit_button.click()
     return browser
 
 
-#def get_projects(browser):
-#    if not os.path.isdir("projects"):
-#        os.mkdir("projects")
-#    os.chdir("projects")
-#
-#    browser.get("https://intra.assistants.epita.fr/projects")
-#    time.sleep(5)
-#    projects = []
-#    to_git = None
-#
-#    while True:
-#        try:
-#            load_button = browser.find_element_by_class_name("MuiButton-outlined")
-#            load_button.click()
-#        except Exception as NoMoreButton:
-#            print("Can't load anymore : {}".format(NoMoreButton))
-#            break
-#        else:
-#            time.sleep(3)
-#
-#    for links in browser.find_elements_by_tag_name("a"):
-#        link = links.get_attribute("href")
-#        if link.startswith("https://intra.assistants.epita.fr/projects/"):
-#            projects.append(link)
-#
-#    while to_git is not True and to_git is not False:
-#        answer = input("git clone the projects? [y/n]: ")
-#        if answer == "y":
-#            to_git = True
-#        if answer == "n":
-#            to_git = False
-#
-#    for project in projects:
-#        get_project(browser, project, to_git)
+def get_homeworks(browser):
+    if not os.path.isdir("homeworks"):
+        os.mkdir("homeworks")
+    os.chdir("homeworks")
+
+    projects = []
+    time.sleep(5)
+
+    course_button = browser.find_element_by_class_name("d2l-dropdown-opener")
+    course_button.click()
+
+for links in browser.find_elements_by_tag_name("a"):
+        link = links.get_attribute("href")
+        if link.startswith("https://intra.assistants.epita.fr/projects/"):
+            projects.append(link)
+
+    for project in projects:
+        get_project(browser, project, to_git)
 
 
 browser = connection()
