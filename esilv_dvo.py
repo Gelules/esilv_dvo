2f69e8504675b20f8495fa1e29ff00be5102a3e8
Jules Aubert <aubertjules@gmail.com>
2019-11-12 11:40:05 +0100

    Check every homeworks and get into the download pop-up.

--- esilv_dvo.py
+++ esilv_dvo.py
@@ -28,16 +28,60 @@ def get_homework(browser, url, name):
         os.mkdir(name)
     os.chdir(name)
 
+    checkbox = browser.find_element_by_name("z_h_cb_sa")
+    checkbox.click()
+
+    dl_button = browser.find_element_by_id("z_u")
+    dl_button.click()
+
+    while True:
+        dl_button = browser.find_element_by_class_name("d2l-button")
+        try:
+            dl_button.click()
+        except Exception:
+            pass
+        else:
+            break
+
+    os.chdir("..")
+
+
+def get_homeworks(browser, url, name):
+    browser.get(url)
+    if not os.path.isdir(name):
+        os.mkdir(name)
+    os.chdir(name)
+
     id = url.split("/")
     id = id[len(id) - 1]
     link = "https://devinci-online.brightspace.com/d2l/lms/dropbox/dropbox.d2l?ou=" + id
     browser.get(link)
 
-    while True:
-        pass
+    hws_url = []
+    hws_name = []
+
+    for links in browser.find_elements_by_tag_name("a"):
+        link = links.get_attribute("href")
+        if "/d2l/lms/dropbox/admin/mark/folder_submissions_users.d2l" in link:
+            hws_url.append(link)
+            hws_name.append(links.get_attribute("innerHTML"))
+
+    for i in range(len(hws_url)):
+        print("[{}] : {}".format(i, hws_name[i]))
+    print("[q] : Quitter")
+
+    choice = input("Choix : ")
+    choice = choice.split(',')
+    if 'q' in choice:
+        return
+
+    for c in choice:
+        get_homework(browser, hws_url[int(c)], hws_name[int(c)])
+
+    os.chdir("..")
 
 
-def get_homeworks(browser):
+def get_courses(browser):
     if not os.path.isdir("homeworks"):
         os.mkdir("homeworks")
     os.chdir("homeworks")
@@ -60,12 +104,14 @@ def get_homeworks(browser):
     print("[q] : Quitter")
 
     choice = input("Choix : ")
-    if choice == "q":
+    choice = choice.split(',')
+    if 'q' in choice:
         return
 
-    get_homework(browser, courses_url[int(choice)], courses_name[int(choice)])
+    for c in choice:
+        get_homeworks(browser, courses_url[int(c)], courses_name[int(c)])
 
 
 browser = connection()
-get_homeworks(browser)
+get_courses(browser)
 browser.quit()
